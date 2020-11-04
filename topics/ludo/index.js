115113bff09ac7d8902ab7b714f2b05cdcc895da
Shubham Rautela <rautelashubham@gmail.com>
2020-11-04 19:47:33 +0000

    Update index.js

--- topics/ludo/index.js
+++ topics/ludo/index.js
@@ -28,11 +28,14 @@ drawSquare(0,0, 6,"rgb(183, 36, 36)");
 drawSquare(378,0,6,"rgb(27, 140, 27)");
 drawSquare(0,378,6, "rgb(27, 84, 140)");
 drawSquare(378,378,6,"rgb(182, 182, 32)");
+drawStars();
 
-strokeStar(dx+274,116,10,5,2);
-strokeStar(dx+106,368,10,5,2);
-strokeStar(dx+358,536,10,5,2);
-strokeStar(dx+526,284,10,5,2);
+function drawStars(){
+  strokeStar(dx+273,116,10,5,2);
+  strokeStar(dx+105,368,10,5,2);
+  strokeStar(dx+357,536,10,5,2);
+  strokeStar(dx+525,284,10,5,2);
+}
 
 // squares for different players
 function drawSquare(x,y, length, color){
@@ -72,17 +75,36 @@ function strokeStar(x, y, r, n, inset) {
 // }
 
 function drawPiece(piece){
+
 console.log(piece.radius+ "   "+ piece.x+ " " +piece.y);
   if(piece.curr>0)
   ctx.clearRect(dx+(piece.path[piece.curr-1][0]-1)*42+1,dy+(piece.path[piece.curr-1][1]-1)*42+1,40,40);
+  drawStars();
   ctx.beginPath();
   ctx.fillStyle = piece.color;
   ctx.strokeStyle = "black";
   ctx.lineWidth = 2;
   ctx.arc(dx+42*(piece.path[piece.curr][0]-1)+21,dy+42*(piece.path[piece.curr][1]-1)+21,piece.radius,0,Math.PI*2);
   ctx.fill();
-  piece.curr++;
 
+
+}
+
+
+async function movePiece(piece, steps){
+  for(let i = 1;i<=steps;i++){
+    await sleep(300);
+    drawPiece(piece);
+    piece.curr++;
+  }
+}
+
+async function goBack(piece){
+  for(;piece.curr>=0;piece.curr--){
+
+    await sleep(100);
+    drawPiece(piece);
+  }
 }
 // let rPiece1 = new Piece(100,100,"red");
 // let rPiece2 = new Piece(100,150,"red");
@@ -95,40 +117,11 @@ console.log(piece.radius+ "   "+ piece.x+ " " +piece.y);
 
 
 
-class Piece{
-  constructor(color){
-  this.radius = 15;
-  this.color = color;
-  this.clickable = false;
-  this.path = [];
- }
-}
 
 
-class RedPiece extends Piece{
-  constructor(x,y){
-    super("red");
-    this.x = x;
-    this.y = y;
-    this.initX = 0;
-    this.initY = 0;
-    this.curr=0;
-    this.path = [ [2,7],[3,7],[4,7],[5,7],[6,7],
-                  [7,6],[7,5],[7,4],[7,3],[7,2],[7,1],
-                  [8,1],
-                  [9,1],[9,2],[9,3],[9,4],[9,5],[9,6],
-                  [10,7],[11,7],[12,7],[13,7],[14,7],[15,7],
-                  [15,8],
-                  [15,9],[14,9],[13,9],[12,9],[11,9],[10,9],
-                  [9,10],[9,11],[9,12],[9,13],[9,14],[9,15],
-                  [8,15],
-                  [7,15],[7,14],[7,13],[7,12],[7,11],[7,10],
-                  [6,9],[5,9],[4,9],[3,9],[2,9],[1,9],
-                  [1,8],[2,8],[3,8],[4,8],[5,8],[6,8],[7,8]
-                ]
-  }
+
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
-let rPiece1 = new RedPiece(100,100);
-canvas.addEventListener('click', function(e){
-  drawPiece(rPiece1);
-})
